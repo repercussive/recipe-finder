1e1714036abfcf560241be80e87a700f8dedec8e
Liam Robertson <liam2robertson@gmail.com>
2024-03-11 22:11:00 +0000

    Add constraints to ingredient names

--- src/main/java/com/repercussive/recipefinder/models/Ingredient.java
+++ src/main/java/com/repercussive/recipefinder/models/Ingredient.java
@@ -15,6 +15,6 @@ public class Ingredient {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ingredient_id_seq")
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false, length = 100)
     private String name;
 }
