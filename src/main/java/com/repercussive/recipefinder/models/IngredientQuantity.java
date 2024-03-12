d49b1bc90dba97ce568ddaa5844b94ebc3973a26
Liam Robertson <liam2robertson@gmail.com>
2024-03-12 12:15:28 +0000

    Add IngredientQuantity entity

---
+++ src/main/java/com/repercussive/recipefinder/models/IngredientQuantity.java
@@ -0,0 +1,29 @@
+package com.repercussive.recipefinder.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name= "recipe_ingredient_quantity")
+public class IngredientQuantity {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipe;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id")
+    private Ingredient ingredient;
+
+    @Column(name = "quantity_per_portion", nullable = false)
+    private Double quantityPerPortion;
+
+    @Column(name = "quantity_unit_name")
+    private String quantityUnitName;
+}
