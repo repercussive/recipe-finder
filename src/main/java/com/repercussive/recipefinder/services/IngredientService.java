be453ede6faf71d312b5c50d067ec3ebbed58736
Liam Robertson <liam2robertson@gmail.com>
2024-03-12 14:14:41 +0000

    Enable caching of ingredients

--- src/main/java/com/repercussive/recipefinder/services/IngredientService.java
+++ src/main/java/com/repercussive/recipefinder/services/IngredientService.java
@@ -1,10 +1,17 @@
 package com.repercussive.recipefinder.services;
 
 import com.repercussive.recipefinder.models.Ingredient;
+import org.springframework.cache.annotation.Cacheable;
+
 import java.util.Optional;
 
 public interface IngredientService {
+    Ingredient createIngredient(Ingredient ingredient);
+
+    @Cacheable("ingredients")
     Optional<Ingredient> findById(Long id);
+
+    @Cacheable("ingredients")
 
     Optional<Ingredient> findByName(String name);
 }
